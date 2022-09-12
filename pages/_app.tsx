e19ffa63905f7d97675d7da8a8fc57010dd2b2ec
Rick Brown <ricky.brown.00@gmail.com>
2022-09-12 18:43:34 +0100

    adds chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from '../theme'
+import Layout from '../components/Layout'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	return (
+		<ChakraProvider theme={theme}>
+			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</ChakraProvider>
+	)
 }
 
 export default MyApp
