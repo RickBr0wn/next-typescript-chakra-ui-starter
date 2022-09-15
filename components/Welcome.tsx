85042e4f7e314988397e664c0e1e7f46f9adcb67
Rick Brown <ricky.brown.00@gmail.com>
2022-09-15 15:15:49 +0100

    demonstrates adaptive color from custom theme

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,14 +1,32 @@
-import { Code, Flex, Heading, Spacer, Text } from '@chakra-ui/react'
+import {
+	Code,
+	Flex,
+	Heading,
+	Spacer,
+	Text,
+	useColorMode
+} from '@chakra-ui/react'
 
 const Welcome = (): JSX.Element => {
+	const { colorMode } = useColorMode()
+	const paperColor =
+		colorMode === 'light' ? 'brand.paper.light' : 'brand.paper.dark'
 	return (
 		<Flex flexDir={'column'} align={'center'} h={'calc(100vh - 80px)'}>
 			{/* 80px is the height of the navbar */}
 			<Spacer />
-			<Heading>Welcome to this</Heading>
-			<Heading color={'green.400'}> Next.js Typescript & Chakra-UI</Heading>
-			<Heading>starter</Heading>
-			<Flex flexDir={'column'} align={'center'} margin={'20px 0'}>
+			<Flex
+				flexDir={'column'}
+				bg={paperColor}
+				p={6}
+				borderRadius={'lg'}
+				align={'center'}
+			>
+				<Heading>Welcome to this</Heading>
+				<Heading color={'green.400'}> Next.js Typescript & Chakra-UI</Heading>
+				<Heading>starter</Heading>
+			</Flex>
+			<Flex flexDir={'column'} align={'center'} margin={'20px 0'} gap={1}>
 				<Text mb={4}>Get started by editing:</Text>
 				<Code>pages/index.tsx</Code>
 				<Code>theme/theme.ts</Code>
