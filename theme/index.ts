b1815054c7cfba42648f605195c8b4a5932da07d
Rick Brown <ricky.brown.00@gmail.com>
2022-09-15 15:15:10 +0100

    improves custom theme

--- theme/index.ts
+++ theme/index.ts
@@ -1,12 +1,64 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme, StyleFunctionProps } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
 
-const theme = extendTheme({
-	colors: {
-		brand: {
-			100: '#f7fafc',
-			900: '#1a202c'
+const breakpoints = {
+	// define custom breakpoints
+	// sm: '30em',
+	// md: '48em',
+	// lg: '62em',
+	// xl: '80em'
+}
+
+const colors = {
+	// add custom colors
+	brand: {
+		paper: {
+			light: 'white',
+			dark: '#292B2F'
 		}
+		// 	100: 'red',
+		// 	200: 'red',
+		// 	300: 'red',
+		// 	400: 'red',
+		// 	500: 'red',
+		// 	600: 'red',
+		// 	700: 'red',
+		// 	800: 'red',
+		// 	900: 'red'
 	}
-})
+}
+
+const config = {
+	// adjust the config of the light/dark mode
+	initialColorMode: 'dark',
+	useSystemColorMode: true
+}
+
+const fonts = {
+	// add custom fonts
+	// body: `Inter,"Segoe UI"...`,
+	// heading: `Inter,"Segoe UI"...`
+}
+
+const styles = {
+	// add global styles
+	global: (props: StyleFunctionProps) => ({
+		body: {
+			// example of changing both the background &
+			// foreground color based on the color mode
+			bg: mode('#F6F6F6', '#36393F')(props),
+			color: mode('#292B30', '#ffffff')(props)
+		},
+		h1: {
+			// example of changing a particular element's style
+			// fontSize: '6xl',
+			// color: mode('red.900', 'red.100')(props)
+		}
+	})
+}
+
+const overrides = { breakpoints, colors, config, fonts, styles }
+
+const theme = extendTheme(overrides)
 
 export default theme
